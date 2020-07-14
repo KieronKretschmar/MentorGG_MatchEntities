44e0775826273a985c3c9516a2f02e5be6c4c19d
Kieron <kieron@mentor.gg>
2020-07-14 16:15:32 +0200

    Extend BombPlant and BombDefused to implement interfaces and add props so they also hold for aborted attempts

--- MatchEntities/Models/BombPlant.cs
+++ MatchEntities/Models/BombPlant.cs
@@ -7,16 +7,44 @@ using System.Numerics;
 
 namespace MatchEntities
 {
+    /// <summary>
+    /// Holds data for an attempted BombPlant.
+    /// </summary>
     public partial class BombPlant :
         IMatchDataEntity,
         IRoundEntity,
-        IIngameEvent
+        IIngameEvent,
+        IPlayerEvent
     {
         public long MatchId { get; set; }
         public byte Round { get; set; }
+        public int StartTime { get; set; }
+
+        /// <summary>
+        /// The time at which the plant attempt finished, either by success or aborting.
+        /// </summary>
         public int Time { get; set; }
         public long PlayerId { get; set; }
         public BombSite Site { get; set; }
+        public bool Success { get; set; }
+        public bool IsCt { get; set; }
+        private float PlayerPosX { get; set; }
+        private float PlayerPosY { get; set; }
+        private float PlayerPosZ { get; set; }
+        public Vector3 PlayerPos
+        {
+            get
+            {
+                return new Vector3(PlayerPosX, PlayerPosY, PlayerPosZ);
+            }
+
+            set
+            {
+                PlayerPosX = value.X;
+                PlayerPosY = value.Y;
+                PlayerPosZ = value.Z;
+            }
+        }
         private float PosX { get; set; }
         private float PosY { get; set; }
         private float PosZ { get; set; }
